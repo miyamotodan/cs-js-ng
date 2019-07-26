297bf518efe99476bf7e78465deaca58197ae3e5
Del Pinto Daniele <d.delpinto@almawave.it>
2019-07-26 16:54:32 +0200

    rifacimento card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() name: string;
+  @Input() icon: string;
+  @Input() type: string;
+
+  private value:number;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.value=this.compute(this.type);
+
   }
+
+  compute = (type) => {
+    
+    var res = 0;
+    res = Math.round(Math.random() * 10);
+    switch (type) {
+      case 'node-count':
+        //conteggio nodi
+        break;
+      case 'edge-count':
+        //conteggio archi
+        break;
+    }
+    return res;
+  }
+
+
+
 
 }
